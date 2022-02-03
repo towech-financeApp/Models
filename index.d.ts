7b2b644d598fbd6b3a55c7d9c36f58dd168dcc47
Tow96 <jose.towe@gmail.com>
2022-02-02 20:27:06 -0600

    Added Category to the index

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,10 @@
 import Transaction from './transaction';
 import User from './user';
 import Wallet from './wallet';
+import Category from './category';
 
 export { 
+  Category,
   Transaction,
   User,
   Wallet,
