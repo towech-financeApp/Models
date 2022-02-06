2a4e4f98277378c61599eead5ee1ef971a1e90e6
Tow96 <jose.towe@gmail.com>
2022-02-06 12:50:59 -0600

    Added category to the transaction model

--- transaction.d.ts
+++ transaction.d.ts
@@ -1,7 +1,10 @@
+import Category from "./category";
+
 export default interface Transaction {
   _id: string;
   user_id: string;
   wallet_id: string;
+  category: Category;
   concept: string;
   amount: number;
   transactionDate: Date;
