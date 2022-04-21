5967918206d063cb95aedc51e75231410479c84b
Tow96 <jose.towe@gmail.com>
2022-04-21 15:56:14 -0500

    Added the possibility for a transaction to have a transfer_id

--- Objects/transaction.d.ts
+++ Objects/transaction.d.ts
@@ -4,6 +4,7 @@ export default interface Transaction {
   _id: string;
   user_id: string;
   wallet_id: string;
+  transfer_id?: string;
   category: Category;
   concept: string;
   amount: number;
