ea5690128f500df521389488d69f8fb85864c05a
Tow96 <jose.towe@gmail.com>
2022-04-30 11:13:03 -0500

    Added edit model response

--- responses.d.ts
+++ responses.d.ts
@@ -1,6 +1,10 @@
 import { AuthenticationResponse, GetCategoriesResponse } from './Responses/webApi';
+import { EditTransactionResponse } from './Responses/TransactionService';
 
 export {
+  // TransactionService
+  EditTransactionResponse,
+
   // WebApi
   AuthenticationResponse,
   GetCategoriesResponse,
