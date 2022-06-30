935e4d37d351716538438b88c46c0aa2f0ef1ace
Tow96 <jose.towe@gmail.com>
2022-06-30 11:43:33 -0500

    Exposed the Debt model

--- objects.d.ts
+++ objects.d.ts
@@ -1,10 +1,12 @@
+import Category from './Objects/category';
+import Debt from './Objects/debt';
 import Transaction from './Objects/transaction';
 import User from './Objects/user';
 import Wallet from './Objects/wallet';
-import Category from './Objects/category';
 
 export { 
   Category,
+  Debt,
   Transaction,
   User,
   Wallet,
